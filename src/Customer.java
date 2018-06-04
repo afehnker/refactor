fd0e30717537d89981f1a1bc313e9e5e8e9dd611
Ansgar <fehnker@gmail.com>
2018-06-04 11:00:56 +0200

    Moved getChARGE

--- src/Customer.java
+++ src/Customer.java
@@ -26,7 +26,7 @@ class Customer {
 			double thisAmount = 0;
 			Rental each = (Rental) rentals.nextElement();
 			// determine amounts for each line
-			thisAmount = getCharge(each);
+			thisAmount = each.getCharge();
 			// add frequent renter points
 			frequentRenterPoints++;
 
@@ -42,25 +42,5 @@ class Customer {
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
 		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
 		return result;
-	}
-
-	private double getCharge( Rental each) {
-		double thisAmount = 0;
-		switch (each.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			thisAmount += 2;
-			if (each.getDaysRented() > 2)
-				thisAmount += (each.getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			thisAmount += each.getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			thisAmount += 1.5;
-			if (each.getDaysRented() > 3)
-				thisAmount += (each.getDaysRented() - 3) * 1.5;
-			break;
-		}
-		return thisAmount;
 	}
 }
